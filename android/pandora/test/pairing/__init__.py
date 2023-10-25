634a304e775ee8ca4fe3bce6e934ac0de2ac5d4b
Hui Peng <phui@google.com>
2023-10-25 13:46:11 -0700

    Add bredr/ssp pairing tests (display_yesno)
    Bug: 277282212
    Test: atest avatar
    Flag: EXEMPT, Tests
    Change-Id: Ib5fe2130b27c769ef9953ba1fa729fc6e943812a

--- android/pandora/test/pairing/__init__.py
+++ android/pandora/test/pairing/__init__.py
@@ -15,11 +15,13 @@
 from pairing.br_edr.legacy.tests import BREDRLegacyTestClass
 from pairing.br_edr.misc.service_access_tests import ServiceAccessTempBondingTest
 
+from pairing.br_edr.ssp.display_output_and_yes_no_input.tests import BREDRDisplayYesNoTestClass
 from pairing.br_edr.ssp.no_output_no_input.tests import BREDRNoOutputNoInputTestClass
 
 from pairing.smp_test import SmpTest
 
 _test_class_list = [
+    BREDRDisplayYesNoTestClass,
     BREDRNoOutputNoInputTestClass,
     BREDRLegacyTestClass,
     ServiceAccessTempBondingTest,
