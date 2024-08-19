ec85141fc3bb17dc4bc2112a59fa5f439066fa8b
Jakub Pawlowski <jpawlowski@google.com>
2024-08-19 15:30:23 +0200

    Fix LE service discovery event cancelling SDP discovery
    When disconnection or timeout event happens for LE service discovery, it
    would now clean up whole control block, including classic SDP things.
    If SDP is taking long time, i.e. due to problems with establishing
    classic connection, this might cause service discovery to return partial
    results.
    
    Test: Bond with dual mode device that fails to connect on classic transport
    Bug: 357902043
    Bug: 360936118
    Flag: com.android.bluetooth.flags.fix_le_evt_cancelling_sdp_discovery
    Change-Id: Ic16c0d58815d6c1260dcf4f3250226c532d4f99f

--- system/bta/dm/bta_dm_disc.cc
+++ system/bta/dm/bta_dm_disc.cc
@@ -585,7 +585,13 @@ static void bta_dm_gatt_disc_complete(uint16_t conn_id, tGATT_STATUS status) {
     } else {
       bta_dm_discovery_cb.conn_id = GATT_INVALID_CONN_ID;
     }
-    bta_dm_discovery_set_state(BTA_DM_DISCOVER_IDLE);
+    if (com::android::bluetooth::flags::fix_le_evt_cancelling_sdp_discovery() &&
+        (bta_dm_discovery_cb.transports & BT_TRANSPORT_BR_EDR)) {
+      log::info("classic discovery still pending {}", bta_dm_discovery_cb.peer_bdaddr);
+      return;
+    } else {
+      bta_dm_discovery_set_state(BTA_DM_DISCOVER_IDLE);
+    }
     bta_dm_execute_queued_discovery_request();
   }
 }
