0d58c3230eae74c654119f452fe72e07bf23d4ea
William Escande <wescande@google.com>
2024-08-21 22:14:02 +0000

    Revert^2 "InitFlags: Remove hci_adapter"
    This reverts commit 75083ae27288b11ec9e403571bdf263501b25c7f.
    
    Reason for revert: relanding after addressing initial issue
    
    InitFlags: Remove hci_adapter
    
    Keep the workaround for floss as this is a floss only behavior
    
    Bug: 290844854
    Change-Id: If1d5b407d5484b6f166384b191c90c65092a2b35
    Fix: 290844854
    Test: m Bluetooth
    Flag: EXEMPT, no logical change

--- system/gd/metrics/chromeos/metrics_event.cc
+++ system/gd/metrics/chromeos/metrics_event.cc
@@ -25,13 +25,14 @@
 #include <map>
 #include <utility>
 
-#include "common/init_flags.h"
 #include "hci/hci_packets.h"
 #include "include/hardware/bluetooth.h"
 #include "include/hardware/bt_av.h"
 #include "include/hardware/bt_hf.h"
 #include "include/hardware/bt_hh.h"
 #include "stack/include/hci_error_code.h"
+
+extern int GetAdapterIndex();
 
 namespace bluetooth {
 namespace metrics {
@@ -673,7 +674,7 @@ static int64_t GetChipsetInfoId(const char* path, const char* file) {
 
 static std::string GetChipsetInfoModuleName() {
   std::string module;
-  int adapter_index = bluetooth::common::InitFlags::GetAdapterIndex();
+  int adapter_index = GetAdapterIndex();
   std::string path = base::StringPrintf(kChipsetInfoModaliasPath, adapter_index);
 
   if (base::ReadFileToString(base::FilePath(path), &module)) {
@@ -686,7 +687,7 @@ static MetricTransportType GetChipsetInfoTransport(void) {
   MetricTransportType transport = MetricTransportType::TRANSPORT_TYPE_UNKNOWN;
   base::FilePath module_realpath;
   std::string module_name;
-  int adapter_index = bluetooth::common::InitFlags::GetAdapterIndex();
+  int adapter_index = GetAdapterIndex();
   std::string path = base::StringPrintf(kChipInfoModuleDirPath, adapter_index);
 
   // examples of module_realpath: /sys/module/btusb and /sys/module/hci_uart
