miyu-xu/packages-modules-Bluetooth

GattService: Fix server requests
When dual mode devices, which is using Identity address on both
transports, connects GATT on both directions then GATT response from the
GATT servers implemented in JAVA might not be multiplexed correctly
which  will lead to blocking ATT channel and eventually to ACL
disconnect due to GATT timeout.

Root cause for this is that mServerMap in GattService might contain two
entries for the same address but different connId, and when sendResponse
is received by the GattService, code does no guarante that correct
connId will be restored before sending response down to the native
stack.

This patch fixes it.

Bug: 356319811
Bug: 361331170
Test: manual tests, connecting GATT over two transports and checking
GATT operations.
Flag: com.android.bluetooth.flags.gatt_server_requests_fix

Change-Id: I77a0b2a31413ee90c0c168567705ec4a38508d43

android/app/src/com/android/bluetooth/gatt/GattService.java
@@ -2913,7 +2913,7 @@ public class GattService extends ProfileService {
             return;
         }
 
-        mHandleMap.addRequest(transId, handle);
+        mHandleMap.addRequest(connId, transId, handle);
 
         ContextMap<IBluetoothGattServerCallback>.App app = mServerMap.getById(entry.serverIf);
         if (app == null) {
@@ -2944,7 +2944,7 @@ public class GattService extends ProfileService {
             return;
         }
 
-        mHandleMap.addRequest(transId, handle);
+        mHandleMap.addRequest(connId, transId, handle);
 
         ContextMap<IBluetoothGattServerCallback>.App app = mServerMap.getById(entry.serverIf);
         if (app == null) {
@@ -2985,7 +2985,7 @@ public class GattService extends ProfileService {
             return;
         }
 
-        mHandleMap.addRequest(transId, handle);
+        mHandleMap.addRequest(connId, transId, handle);
 
         ContextMap<IBluetoothGattServerCallback>.App app = mServerMap.getById(entry.serverIf);
         if (app == null) {
@@ -3027,7 +3027,7 @@ public class GattService extends ProfileService {
             return;
         }
 
-        mHandleMap.addRequest(transId, handle);
+        mHandleMap.addRequest(connId, transId, handle);
 
         ContextMap<IBluetoothGattServerCallback>.App app = mServerMap.getById(entry.serverIf);
         if (app == null) {
@@ -3311,15 +3311,26 @@ public class GattService extends ProfileService {
             return;
         }
 
-        Log.v(TAG, "sendResponse() - address=" + address);
+        Log.v(TAG, "sendResponse() - address=" + address + ", requestId=" + requestId);
 
         int handle = 0;
-        HandleMap.Entry entry = mHandleMap.getByRequestId(requestId);
-        if (entry != null) {
-            handle = entry.handle;
-        }
+        Integer connId = 0;
 
-        Integer connId = mServerMap.connIdByAddress(serverIf, address);
+        if (!Flags.gattServerRequestsFix()) {
+            HandleMap.Entry entry = mHandleMap.getByRequestId(requestId);
+            if (entry != null) {
+                handle = entry.handle;
+            }
+            connId = mServerMap.connIdByAddress(serverIf, address);
+        } else {
+            HandleMap.RequestData requestData = mHandleMap.getRequestDataByRequestId(requestId);
+            if (requestData != null) {
+                handle = requestData.handle;
+                connId = requestData.connId;
+            } else {
+                connId = mServerMap.connIdByAddress(serverIf, address);
+            }
+        }
         mNativeInterface.gattServerSendResponse(
                 serverIf,
                 connId != null ? connId : 0,

android/app/src/com/android/bluetooth/gatt/HandleMap.java
@@ -17,6 +17,8 @@ package com.android.bluetooth.gatt;
 
 import android.util.Log;
 
+import com.android.bluetooth.flags.Flags;
+
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -86,18 +88,38 @@ class HandleMap {
         }
     }
 
+    static class RequestData {
+        public int connId;
+        public int handle;
+
+        RequestData(int connId, int handle) {
+            this.connId = connId;
+            this.handle = handle;
+        }
+    }
+    ;
+
     List<Entry> mEntries = null;
     Map<Integer, Integer> mRequestMap = null;
+    Map<Integer, RequestData> mEnhancedRequestMap = null;
     int mLastCharacteristic = 0;
 
     HandleMap() {
         mEntries = new CopyOnWriteArrayList<Entry>();
-        mRequestMap = new ConcurrentHashMap<Integer, Integer>();
+        if (!Flags.gattServerRequestsFix()) {
+            mRequestMap = new ConcurrentHashMap<Integer, Integer>();
+        } else {
+            mEnhancedRequestMap = new ConcurrentHashMap<Integer, RequestData>();
+        }
     }
 
     void clear() {
         mEntries.clear();
-        mRequestMap.clear();
+        if (!Flags.gattServerRequestsFix()) {
+            mRequestMap.clear();
+        } else {
+            mEnhancedRequestMap = new ConcurrentHashMap<Integer, RequestData>();
+        }
     }
 
     void addService(
@@ -170,16 +192,31 @@ class HandleMap {
         return mEntries;
     }
 
-    void addRequest(int requestId, int handle) {
-        mRequestMap.put(requestId, handle);
+    void addRequest(int connId, int requestId, int handle) {
+        if (!Flags.gattServerRequestsFix()) {
+            mRequestMap.put(requestId, handle);
+        } else {
+            mEnhancedRequestMap.put(requestId, new RequestData(connId, handle));
+        }
     }
 
     void deleteRequest(int requestId) {
-        mRequestMap.remove(requestId);
+        if (!Flags.gattServerRequestsFix()) {
+            mRequestMap.remove(requestId);
+        } else {
+            mEnhancedRequestMap.remove(requestId);
+        }
     }
 
     Entry getByRequestId(int requestId) {
-        Integer handle = mRequestMap.get(requestId);
+        Integer handle;
+        if (!Flags.gattServerRequestsFix()) {
+            handle = mRequestMap.get(requestId);
+        } else {
+            RequestData data = mEnhancedRequestMap.get(requestId);
+            handle = data.handle;
+        }
+
         if (handle == null) {
             Log.e(TAG, "getByRequestId() - Request ID " + requestId + " not found!");
             return null;
@@ -187,10 +224,28 @@ class HandleMap {
         return getByHandle(handle);
     }
 
+    RequestData getRequestDataByRequestId(int requestId) {
+        RequestData data = mEnhancedRequestMap.get(requestId);
+        if (data == null) {
+            Log.e(TAG, "getRequestDataByRequestId() - Request ID " + requestId + " not found!");
+        } else {
+            Log.d(
+                    TAG,
+                    ("getRequestDataByRequestId(), requestId=" + requestId)
+                            + (", connId=" + data.connId + ",handle=" + data.handle));
+        }
+
+        return data;
+    }
+
     /** Logs debug information. */
     void dump(StringBuilder sb) {
         sb.append("  Entries: ").append(mEntries.size()).append("\n");
-        sb.append("  Requests: ").append(mRequestMap.size()).append("\n");
+        if (!Flags.gattServerRequestsFix()) {
+            sb.append("  Requests: ").append(mRequestMap.size()).append("\n");
+        } else {
+            sb.append("  Requests: ").append(mEnhancedRequestMap.size()).append("\n");
+        }
 
         for (Entry entry : mEntries) {
             sb.append("  ").append(entry.serverIf).append(": [").append(entry.handle).append("] ");

snvd-io/platform_packages_modules_Bluetooth

Test: atest GattServiceTest

android/app/src/com/android/bluetooth/gatt/GattService.java
@@ -2913,7 +2913,7 @@ public class GattService extends ProfileService {
             return;
         }
 
-        mHandleMap.addRequest(transId, handle);
+        mHandleMap.addRequest(connId, transId, handle);
 
         ContextMap<IBluetoothGattServerCallback>.App app = mServerMap.getById(entry.serverIf);
         if (app == null) {
@@ -2944,7 +2944,7 @@ public class GattService extends ProfileService {
             return;
         }
 
-        mHandleMap.addRequest(transId, handle);
+        mHandleMap.addRequest(connId, transId, handle);
 
         ContextMap<IBluetoothGattServerCallback>.App app = mServerMap.getById(entry.serverIf);
         if (app == null) {
@@ -2985,7 +2985,7 @@ public class GattService extends ProfileService {
             return;
         }
 
-        mHandleMap.addRequest(transId, handle);
+        mHandleMap.addRequest(connId, transId, handle);
 
         ContextMap<IBluetoothGattServerCallback>.App app = mServerMap.getById(entry.serverIf);
         if (app == null) {
@@ -3027,7 +3027,7 @@ public class GattService extends ProfileService {
             return;
         }
 
-        mHandleMap.addRequest(transId, handle);
+        mHandleMap.addRequest(connId, transId, handle);
 
         ContextMap<IBluetoothGattServerCallback>.App app = mServerMap.getById(entry.serverIf);
         if (app == null) {
@@ -3311,15 +3311,26 @@ public class GattService extends ProfileService {
             return;
         }
 
-        Log.v(TAG, "sendResponse() - address=" + address);
+        Log.v(TAG, "sendResponse() - address=" + address + ", requestId=" + requestId);
 
         int handle = 0;
-        HandleMap.Entry entry = mHandleMap.getByRequestId(requestId);
-        if (entry != null) {
-            handle = entry.handle;
-        }
+        Integer connId = 0;
 
-        Integer connId = mServerMap.connIdByAddress(serverIf, address);
+        if (!Flags.gattServerRequestsFix()) {
+            HandleMap.Entry entry = mHandleMap.getByRequestId(requestId);
+            if (entry != null) {
+                handle = entry.handle;
+            }
+            connId = mServerMap.connIdByAddress(serverIf, address);
+        } else {
+            HandleMap.RequestData requestData = mHandleMap.getRequestDataByRequestId(requestId);
+            if (requestData != null) {
+                handle = requestData.mHandle;
+                connId = requestData.mConnId;
+            } else {
+                connId = mServerMap.connIdByAddress(serverIf, address);
+            }
+        }
         mNativeInterface.gattServerSendResponse(
                 serverIf,
                 connId != null ? connId : 0,

android/app/src/com/android/bluetooth/gatt/HandleMap.java
@@ -86,13 +86,23 @@ class HandleMap {
         }
     }
 
+    static class RequestData {
+        int mConnId;
+        int mHandle;
+
+        RequestData(int connId, int handle) {
+            mConnId = connId;
+            mHandle = handle;
+        }
+    }
+
     List<Entry> mEntries = null;
-    Map<Integer, Integer> mRequestMap = null;
+    Map<Integer, RequestData> mRequestMap = null;
     int mLastCharacteristic = 0;
 
     HandleMap() {
         mEntries = new CopyOnWriteArrayList<Entry>();
-        mRequestMap = new ConcurrentHashMap<Integer, Integer>();
+        mRequestMap = new ConcurrentHashMap<Integer, RequestData>();
     }
 
     void clear() {
@@ -170,8 +180,8 @@ class HandleMap {
         return mEntries;
     }
 
-    void addRequest(int requestId, int handle) {
-        mRequestMap.put(requestId, handle);
+    void addRequest(int connId, int requestId, int handle) {
+        mRequestMap.put(requestId, new RequestData(connId, handle));
     }
 
     void deleteRequest(int requestId) {
@@ -179,12 +189,31 @@ class HandleMap {
     }
 
     Entry getByRequestId(int requestId) {
-        Integer handle = mRequestMap.get(requestId);
+        Integer handle = null;
+        RequestData data = mRequestMap.get(requestId);
+        if (data != null) {
+            handle = data.mHandle;
+        }
+
         if (handle == null) {
             Log.e(TAG, "getByRequestId() - Request ID " + requestId + " not found!");
             return null;
         }
         return getByHandle(handle);
+    }
+
+    RequestData getRequestDataByRequestId(int requestId) {
+        RequestData data = mRequestMap.get(requestId);
+        if (data == null) {
+            Log.e(TAG, "getRequestDataByRequestId() - Request ID " + requestId + " not found!");
+        } else {
+            Log.d(
+                    TAG,
+                    ("getRequestDataByRequestId(), requestId=" + requestId)
+                            + (", connId=" + data.mConnId + ",handle=" + data.mHandle));
+        }
+
+        return data;
     }
 
     /** Logs debug information. */